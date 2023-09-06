3ddd17fd7c8d8905ba129609e7b6c6f0b98ed251
Ken Papagno <kenneth.papagno@concentrix.com>
2023-09-06 14:01:12 -0400

    SC S3.9 DynamoDB.2

--- deployment/sync-to-s3.sh
+++ deployment/sync-to-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Functions to reduce repetitive code
 # do_cmd will exit if the command has a non-zero return code.
-release='v2.0.1.cnxc.1'
+release='v2.0.1.cnxc.3'
 
 do_cmd () {
     echo "------ EXEC $*"
